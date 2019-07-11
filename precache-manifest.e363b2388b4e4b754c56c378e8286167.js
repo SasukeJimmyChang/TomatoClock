2f4c416679065b818046e2c81be0c3a34f771e8a
JimmyChang <jimmy.chang@velocitylight.net>
2019-07-11 23:40:05 +0800

    Updates

---
+++ precache-manifest.e363b2388b4e4b754c56c378e8286167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94284c4cdaa7fadc4f44f12f14f9aca1",
+    "url": "/TomatoClock/index.html"
+  },
+  {
+    "revision": "df4fd23d4e015f0940f6",
+    "url": "/TomatoClock/static/css/main.87916bd9.chunk.css"
+  },
+  {
+    "revision": "962a78f2e47cb9bd9f1f",
+    "url": "/TomatoClock/static/js/2.fdc00fc9.chunk.js"
+  },
+  {
+    "revision": "df4fd23d4e015f0940f6",
+    "url": "/TomatoClock/static/js/main.a924e72f.chunk.js"
+  },
+  {
+    "revision": "569330dc5f324a626693",
+    "url": "/TomatoClock/static/js/runtime~main.cae2c59d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TomatoClock/static/media/logo.5d5d9eef.svg"
+  }
+]);
